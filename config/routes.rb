bffb833b16ed0ca86179a1d69bc2d04036784b2e
Mai Le <mle@godaddy.com>
2015-04-17 13:17:27 -0700

    Add link to run test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'application#index'
   get 'run-test' => 'application#run_test'
 
   # The priority is based upon order of creation: first created -> highest priority.
