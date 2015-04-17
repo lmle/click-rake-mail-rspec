058e5f58e33a9b402434fda659523d8c88a7a0ca
ndsouza <ndsouza@godaddy.com>
2015-04-17 16:54:34 -0700

    remember to change the location for the todo_array for this to work. now it points to a location on my machine

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-source 'https://rubygems.org'
+source 'http://rubygems.org'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
@@ -38,12 +38,13 @@ gem  'rspec','~> 3.0.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug'
 
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+
+  gem 'tzinfo-data'
 end
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
+    @todo_array = get_test_cases_hash('C:/Users/ndsouza/Documents/horizontal_contextmenu/wsb-ruby-automation')[:data]
     render 'list-tests'
+  end
+
+  def todo_array
+    #@todo_array = [ "Buy Milk", "Buy Soap", "Pay bill", "Draw Money" ]
   end
 
   def run_test
@@ -17,5 +22,59 @@ class ApplicationController < ActionController::Base
     options[:rails_env] ||= Rails.env
     args = options.map { |n, v| "#{n.to_s.upcase}='#{v}'" }
     system "/usr/bin/rake #{task} #{args.join(' ')} &"
+  end
+
+  def get_line_with_data(input_file,data)
+    line_info = {:string_data=>[],:line_number=>[]}
+    file = File.open(input_file.first, 'r+')
+    lines_array =[]
+    file.each do |line|
+      lines_array << line
+    end
+    lines_array.each_with_index do |line, i|
+      if (line.match(data))
+        line_info[:string_data] << line.match(data)[1].strip
+        line_info[:line_number] << i
+        if data == /describe(.*)do/m
+          return line_info
+        end
+      end
+    end
+    return line_info
+  end
+
+  def get_test_cases_hash(project_location)
+    master_hash ={}
+    test_count = 0
+    master_hash[:data] = {}
+    test_domain_count = 0
+    Dir.glob("#{project_location}/spec/**/*.rb").each do |specfile| # enumerate the Spec files
+      file_text = File.read(specfile) # downcase here if necessary
+      master_hash[:data][test_domain_count] = {}
+      file_name = File.basename(specfile)
+      file_path =  Dir.glob("#{project_location}/**/#{file_name}")
+      if !file_text.include?("# RSpec.describe")
+        file_name = File.basename(specfile)
+        file_path =  Dir.glob("#{project_location}/**/#{file_name}")
+        describe = get_line_with_data(file_path,/describe(.*)do/m)
+        master_hash[:data][test_domain_count][:domain_name] = describe[:string_data][0]
+        master_hash[:data][test_domain_count][:tests] =[]
+
+        test_name = get_line_with_data(file_path,/it (.*) do/m)
+        test_name[:string_data].each_with_index do |name,i|
+          master_hash[:data][test_domain_count][:tests][i] = {}
+          master_hash[:data][test_domain_count][:tests][i][:test_name] = name
+          master_hash[:data][test_domain_count][:tests][i][:line_number] = test_name[:line_number][i]
+          master_hash[:data][test_domain_count][:tests][i][:path] =  file_path
+          test_count += 1
+        end
+      end
+      if !master_hash[:data][test_domain_count].empty? && !master_hash[:data][test_domain_count][:tests].empty?
+        test_domain_count +=1
+      end
+    end
+    master_hash[:number_of_specs] = test_domain_count
+    master_hash[:total_number_of_tests] = test_count
+    master_hash
   end
 end
