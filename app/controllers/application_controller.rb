ccbd9df8c0cdc01064128b580d870e39c63252d7
Mai Le <mle@godaddy.com>
2015-04-17 11:20:09 -0700

    Add rake command infrastructure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def run_test
+    call_rake(:run_test)
+  end
+
+  private
+
+  def call_rake(task, options = {})
+    options[:rails_env] ||= Rails.env
+    args = options.map { |n, v| "#{n.to_s.upcase}='#{v}'" }
+    system "/usr/bin/rake #{task} #{args.join(' ')} &"
+  end
 end
---
+++ lib/tasks/run_test.rake
@@ -0,0 +1,4 @@
+desc "Run Test"
+task :run_test => :environment do
+  # TODO: Fill in rake task for running the test and emailing the HTML report
+end
